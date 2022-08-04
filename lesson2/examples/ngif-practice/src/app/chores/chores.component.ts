178360bdafe8f907466d1e9aa16e09d5131207a2
Linda Ryan <lwryan6@gmail.com>
2022-08-03 23:56:17 -0500

    Completed ngif practice.

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   // Chptr 30.3.2 Try It 1/2 DOES display "Work harder!" html element:
+   // chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
+   // finishedChores = [];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   // Chptr 30.3.2 Try It 1/2 does NOT display "Work harder!" html element:
+   // chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
+   // finishedChores = ['Clean the bathrooms', 'Vacuum', 'Dust', 'Organize your sock drawer'];
+   
+   // Chptr 30.3.2 Try It 3/4 display image:
+   // chores = ['Empty dishwasher'];
+   // finishedChores = ['Clean the bathrooms', 'Vacuum', 'Dust', 'Organize your sock drawer'];
+   
+   // Chptr 30.3.2 Try It 3/4 displays "No allowance yet.":
+   // chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
+   // finishedChores = ['Clean the bathrooms', 'Vacuum', 'Dust', 'Organize your sock drawer'];
+
+   // Chptr 30.3.2 Try It 5 display h1 "Ice cream treat!":
+   chores = ['Empty dishwasher'];
+   finishedChores = ['Clean the bathrooms', 'Vacuum', 'Dust', 'Organize your sock drawer'];
+
+   // Chptr 30.3.2 Try It 5 display h3/p on how to earn ice cream:
+   // chores = [];
+   // finishedChores = ['Clean the bathrooms', 'Vacuum', 'Dust', 'Organize your sock drawer'];
+
+   targetImage = 'https://pbs.twimg.com/profile_images/1295809711393177606/zJcb95MI_400x400.jpg';
 
    constructor() { }
 
