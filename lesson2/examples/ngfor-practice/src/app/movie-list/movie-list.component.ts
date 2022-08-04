6edae01ed35a32dc24581037a1bc9a717e5ebe0e
Linda Ryan <lwryan6@gmail.com>
2022-08-03 22:20:36 -0500

    completed ngfor practice

--- lesson2/examples/ngfor-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/ngfor-practice/src/app/movie-list/movie-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
   movies = ['The Manchurian Candidate', 'Oceans 8', 'Hidden Figures', 'The Incredibles', 'The Princess Bride'];
+  
+  // Example below can be used to trigger the *ngIf else block in html file. //
+  // movies = ['Hidden Figures', 'The Incredibles', 'The Princess Bride'];
 
   constructor() { }
 
