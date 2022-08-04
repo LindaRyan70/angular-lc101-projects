3a078720f59ce12371469abd1c656ad13049f3e1
Linda Ryan <lwryan6@gmail.com>
2022-08-04 02:40:00 -0500

    Completed angular-lesson2-input-practice.

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,27 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   // addMovie(newTitle: string) {
+   //    if(!this.movies.includes(newTitle) && newTitle !== ''){
+   //       this.movies.push(newTitle);
+   //    }
+   // }
+
+// Lines below replace addMovie() code above to solve Bonus Mission.
+// The errorMessage variable can me any word b/c only applies in this function and returns as empty string to the html message variable in imput/button/p if no error.
+// I added .trim() tp the validation to prevent white space entries.  
+addMovie (newTitle: string) {
+      let errorMessage = '';
+      if(newTitle.trim() === ''){
+         errorMessage = 'Please enter a movie title.';
+      } else if (this.movies.includes(newTitle)) {
+         errorMessage = `${newTitle} is already on the list. Please enter a different movie title.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return errorMessage;
+   }
+
+
 }
